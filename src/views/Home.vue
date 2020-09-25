9b575b9b8254c253949125f3a4b4027e8c5c5519
Ryan Brink <rdbrnk@gmail.com>
2020-09-25 19:54:37 -0400

    removing HelloWorld.vue component, creating Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<article>
+		<h1>User Directory</h1>
+
+		<section>
+			<h2>Welcome to the user directory.</h2>
+			<p>
+				This directory will display a paginated list of user data retrieved from the <a href="http://randomuser.me" target="blank">randomuser.me API</a>.
+			</p>
+
+			<p>
+				You will be able to navigate through the user data.
+			</p>
+
+			<p>
+				Each page will have option to export the user data to a downloadable CSV file.
+			</p>
+		</section>
+	</article>
+</template>
