110148c767f80044ce676ab942d0cb40778a190a
Ryan Brink <rdbrnk@gmail.com>
2020-09-26 15:46:40 -0400

    making Header component sticky

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,22 @@
 <template>
-  <header>
-    <div class="w-2/3mx-auto">
-      <h1 class="font-serif text-2xl lg:text-4xl lg:text-left pt-8 lg:pt-12">User Directory</h1>
+  <header class="sticky-header bg-gray-100">
+    <div class="w-2/3 mx-auto pb-8">
+      <h1 class="text-2xl lg:text-4xl lg:text-left text-center pt-8 lg:pt-12">User Directory</h1>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.sticky-header {
+  position: fixed;
+  right: 0;
+  left: 0;
+  z-index: 999;
+}
+</style>
